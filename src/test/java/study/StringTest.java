c349c673918696bb8d0a29926eb51f819811c034
jhshim0131 <jhshim0131@gmail.com>
2024-10-27 15:04:38 +0900

    test: String test case added

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,21 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+
+    @Test
+    void containsChar() {
+        String sample = "1,2";
+        String[] split = sample.split(",");
+        assertThat(split).contains("1");
+        assertThat(split).contains("2");
+        assertThat(split).containsExactly("1","2");
+    }
+
+    @Test
+    void takeoutParenthesis() {
+        String sample = "(1,2)";
+        String substring = sample.substring(1,4);
+        assertThat(substring).isEqualTo("1,2");
+    }
 }
